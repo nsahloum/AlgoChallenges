06968c2df06c867b2f45f139053047e1ecc88cb6
nsahloum <sahloumi.najima@gmail.com>
2016-06-21 15:13:06 +0100

    day0.rb

---
+++ HackerRank/Day 0: Hello, World.  /day0.rb
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+inputString = gets # get a line of input from stdin and save it to our variable
+gets = "Welcome to 30 Days of Code!"
+# Your first line of output goes here
+puts 'Hello, World.'
+
+# Write the second line of output
+puts inputString
